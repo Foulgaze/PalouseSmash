86802a9ce8a44ea059c69433cef5503e56c9f7f3
GabeRighi <foulgaze@DESKTOP-FNVQKK3.localdomain>
2022-12-29 20:02:59 -0800

    Version 0.1 : Just added very basic template, along with basic vercel data

---
+++ api/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import process from "process";
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const app = express()
+const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.use(express.static(`${__dirname}/../staticFiles`));
+
+app.get('/', (req, res) =>{
+    res.send("{}");
+})
+
+app.get('/rankings', (req, res) => {
+    res.sendFile(`${__dirname}/staticFiles/rankings.html`);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.use(function (req, res, next) {
+    res.status(404).send("Invalid Url")
+  });
